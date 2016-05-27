b01a3ea525a9dd51a851d860f334819b4735563a
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2016-05-27 10:19:03 -0400

    using express

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jdgiguere on 16-05-27.
+ */
+
+var port = process.env['PORT'] || 8080;
+
+var express = require('express');
+
+var main = express();
+
+main.get('/', function(req, res) {
+    res.send('Hello world!')
+});
+
+main.listen(port, function() {
+    console.log('We are running on port ' + port);
+});
