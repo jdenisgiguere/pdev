b984fe9922a695ba8f659519475ab2f3c64f2740
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2016-05-27 14:14:27 -0400

    premier bouton

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ var port = process.env['PORT'] || 8080;
 
 var express = require('express');
 
+var maki = require('maki');
+
 var main = express();
+
+//Static files
+main.use(express.static('static'));
+main.use('/maki', express.static('node_modules/maki'));
 
 main.get('/', function(req, res) {
     res.send('Hello world!')
 });
 
-main.use(express.static('static'));
+
+main.get('/bandes-riveraines/apprendre-plus', function(req, res) {
+    bookIcon = '/maki/icons/' + 'library' + '-15.svg';
+    res.send('<html><head><link rel="stylesheet" type="text/css" href="/css/bouton.css"></head><body><a href="http://banderiveraine.org/" target="_blank" class="lien_bouton"><div class="action_bouton"><div><img src="' +
+        bookIcon + '" width="80px" height="80px"/> <p>En apprendre davantage</p></div></div></a></body></html>')
+});
+
 
 main.listen(port, function() {
     console.log('We are running on port ' + port);
