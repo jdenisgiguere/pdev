0154430dcaa57ef665055c2635009f61d0de080d
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2016-09-01 09:24:33 -0400

    Diagramme de qualité en JSX

--- static/admin/eau_ui.jsx
+++ static/admin/eau_ui.jsx
@@ -31,40 +31,53 @@ var IndicateurSelect = React.createClass({
     }
 });
 
+var Diagramme = React.createClass({
+    render: function() {
+        return (
+            <svg height="220" width="600">
+                <defs>
+                    <linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%" style={{stopColor: "#6985FB", stopOpacity: 1}}/>
+                        <stop offset="100%" style={{stopColor: "#006500", stopOpacity: 1}}/>
+                    </linearGradient>
+                </defs>
+
+
+                <g className="echelle" transform="translate(50,80)">
+                    <rect width="500" height="100" fill="url(#grad2)"/>
+                </g>
+
+                <g className="mesure" transform="translate(50,80)">
+                    <rect x="216" y="50" height="10" width="10" fill="rgb(255,255,0)"/>
+                </g>
+
+                <g className="legende">
+
+                    <text x="50" y="55" fill="black" textAnchor="middle">Ultra-
+                        <tspan x="50" y="75" textAnchor="middle">oligotrophe</tspan>
+                    </text>
+                    <text x="133" y="65" fill="black" textAnchor="middle">Oligotrophe</text>
+                    <path id="oligo-vs-meso" d="M 216 80 L 216 180 Z" stroke="black" strokeWidth="2" fill="none"/>
+                    <text x="216" y="205" textAnchor="middle">10</text>
+                    <text x="300" y="65" fill="black" textAnchor="middle">Mésotrophe</text>
+                    <path id="meso-vs-eu" d="M 382 80 L 382 180 Z" stroke="black" strokeWidth="2" fill="none"/>
+                    <text x="382" y="205" textAnchor="middle">30</text>
+                    <text x="466" y="65" fill="black" textAnchor="middle">Eutrophe</text>
+                    <text x="550" y="55" fill="black" textAnchor="middle">Hyper-
+                        <tspan x="550" y="75" textAnchor="middle">eutrophe</tspan>
+                    </text>
+                </g>
+
+            </svg>
+        );
+    }
+});
+
 var GenererDiagramme = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
-        var result = [
-            '<svg height="220" width="600">',
-                '<defs>',
-                    '<linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">',
-                        '<stop offset="0%" style="stop-color:rgb(105, 133, 251);stop-opacity:1"/>',
-                        '<stop offset="100%" style="stop-color:rgb(0, 101, 0);stop-opacity:1"/>',
-                    '</linearGradient>',
-                '</defs>',
-               '<g class="echelle" transform="translate(50,80)">',
-                    '<rect width="500" height="100" fill="url(#grad2)"/>',
-                '</g>',
-                '<g class="mesure" transform="translate(50,80)">',
-                    '<rect x="216" y="50" height="10" width="10" fill="rgb(255,255,0)"/>',
-                '</g>',
-                '<g class="legende">',
-                    '<text x="50" y="55" fill="black" text-anchor="middle">Ultra-',
-                        '<tspan x="50" y="75" text-anchor="middle">oligotrophe</tspan>',
-                    '</text>',
-                    '<text x="133" y="65" fill="black" text-anchor="middle">Oligotrophe</text>',
-                    '<path id="oligo-vs-meso" d="M 216 80 L 216 180 Z" stroke="black" stroke-width="2" fill="none"/>',
-                    '<text x="216" y="205" text-anchor="middle">10</text>',
-                    '<text x="300" y="65" fill="black" text-anchor="middle">Mésotrophe</text>',
-                    '<path id="meso-vs-eu" d="M 382 80 L 382 180 Z" stroke="black" stroke-width="2" fill="none"/>',
-                    '<text x="382" y="205" text-anchor="middle">30</text>',
-                    '<text x="466" y="65" fill="black" text-anchor="middle">Eutrophe</text>',
-                    '<text x="550" y="55" fill="black" text-anchor="middle">Hyper-',
-                        '<tspan x="550" y="75" text-anchor="middle">eutrophe</tspan>',
-                    '</text>',
-                '</g>',
-            '</svg>',
-        ];
+        var diagramme = ReactDOMServer.renderToStaticMarkup(<Diagramme/>);
+        var result = [diagramme,]
         var blob = new Blob(result, {type: "image/svg+xml;charset=utf-8"});
         saveAs(blob, "qualite_eau.svg");
     },
