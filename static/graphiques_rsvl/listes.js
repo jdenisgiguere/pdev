cad746744a2416cfdc1f9f2fa23f56607d192207
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2016-10-17 10:29:34 -0400

    Navigation des graphiques phosphore et chlorophylle

---
+++ static/graphiques_rsvl/listes.js
@@ -0,0 +1,5 @@
+/**
+ * Created by jdgiguere on 16-10-17.
+ */
+var graphiquePhosphore = ["graphiques_rsvl/demo1.svg", "graphiques_rsvl/demo2.svg", "graphiques_rsvl/demo3.svg"];
+var graphiqueChlorophylle = ["graphiques_rsvl/demo1.svg", "graphiques_rsvl/demo2.svg", "graphiques_rsvl/demo3.svg"];
